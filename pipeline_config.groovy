7e361dcdc09173d72f8f8ef485af9ce23fe98506
Jeffrey Chiang <chiang_jeffrey@bah.com>
2019-01-07 20:28:12 -0500

    Add ppeline config

---
+++ pipeline_config.groovy
@@ -0,0 +1,10 @@
+sdp_image_repository = "http://0.0.0.0:5000"
+sdp_image_repository_credential = "sdp-docker-registry"
+application_image_repository = "0.0.0.0:5000"
+application_image_repository_credential = "sdp-docker-registry"
+
+libraries{
+  sdp
+  github_enterprise
+  docker
+}
